2340485db49f65d58242c5a6c9356cc9634f2ae6
KoalaDostavala <koala.dostavala@mail.ru>
2019-10-25 14:30:32 +0500

    5.7. Ипотека. [#156315]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            percent = amount * percent / 100;
+            amount = percent + amount;
+            amount -= salary;
+            year++;
+        }
+        return year;
+    }
+
+}
