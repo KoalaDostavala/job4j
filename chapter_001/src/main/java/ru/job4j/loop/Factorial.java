a30f47984b11cec1c9ac87e33997effced76fbe8
KoalaDostavala <koala.dostavala@mail.ru>
2019-10-25 14:57:20 +0500

    5.8. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,7 +1,18 @@
 package ru.job4j.loop;
 
+/**
+ * Class Factorial.
+ *
+ * @author Arina Shamsemukhametova.
+ */
 public class Factorial {
 
+    /**
+     * Method calc.
+     * Находит факториал числа.
+     * @param n - число для рассчёта факториала.
+     * @return факториал числа.
+     */
     public int calc(int n) {
         int result = 1;
         for (int number = 1; number <= n; number++) {
---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for tasks with loops.
+ *
+ * @author Arina Shamsemukhametova.
+ * @version 1.
+ * @since 25.10.2019.
+ */
+package ru.job4j.loop;
