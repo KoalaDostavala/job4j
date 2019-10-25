213eec694584503ef17ed07e2671cca436b09e2a
KoalaDostavala <koala.dostavala@mail.ru>
2019-10-25 14:11:05 +0500

    5.6. Простые числа. [#156314]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public int calc(int finish) {
+        int count = finish == 2 ? 1 : finish - 1;
+        for (int i = 2; i <= finish; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+}
